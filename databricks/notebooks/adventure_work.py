c1fa0d0042772bb64b8bb1714aaedfe7ab51f852
anjeereddy <ram@tcs.com>
2026-06-07 23:57:13 +0530

    moved the values to key vault

--- databricks/notebooks/adventure_work.py
+++ databricks/notebooks/adventure_work.py
@@ -10,11 +10,13 @@
 
 # service_credential = dbutils.secrets.get(scope="<secret-scope>",key="<service-credential-key>")
 client_secret = dbutils.secrets.get(scope="adls-scope", key="databricks-client-secret")
+client_id = dbutils.secrets.get(scope="adls-scope", key="client-id")
+tenant_id = dbutils.secrets.get(scope="adls-scope", key="tenant-id")
 spark.conf.set("fs.azure.account.auth.type.valaxystrgaccv2.dfs.core.windows.net", "OAuth")
 spark.conf.set("fs.azure.account.oauth.provider.type.valaxystrgaccv2.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-spark.conf.set("fs.azure.account.oauth2.client.id.valaxystrgaccv2.dfs.core.windows.net", "8b85b72b-8028-4714-99b6-346d0f81c824")
+spark.conf.set("fs.azure.account.oauth2.client.id.valaxystrgaccv2.dfs.core.windows.net", client_id)
 spark.conf.set("fs.azure.account.oauth2.client.secret.valaxystrgaccv2.dfs.core.windows.net", client_secret)
-spark.conf.set("fs.azure.account.oauth2.client.endpoint.valaxystrgaccv2.dfs.core.windows.net", "https://login.microsoftonline.com/573aa466-4a8f-48b6-9a65-961f4f5f7565/oauth2/token")
+spark.conf.set("fs.azure.account.oauth2.client.endpoint.valaxystrgaccv2.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenant_id}/oauth2/token")
 
 # COMMAND ----------
 
